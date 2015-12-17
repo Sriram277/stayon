bc89e226e2f04678551fb366f8727dad2df37c88
unknown <sriramakiran7@gmail.com>
2015-12-17 07:48:26 +0530

    Modified widget file

--- routes/widget.js
+++ routes/widget.js
@@ -6,20 +6,32 @@ var mongoose = require('mongoose');
 var Widget = require('../models/widget')(mongoose);
 
 
-// Multiple files upload
 router.post('/save', action_save_widgets);
 
 router.get('/list/files', action_list_widgets);
 
 router.delete('/delete/:id', action_delete_widget);
 
-function action_save_widgets(){
+function action_save_widgets(req, res) {
 
+    var Widget = new Widget(req.body);
+    widget.save(function (err, widget) {
+        if (err || !widget) {
+            res.status(500).send(err);
+        }
+        else {
+
+            return res.json({
+                files: widget
+            });
+
+        }
+    });
 }
 
 function action_list_widgets(req, res, next) {
     console.log(req.query);
-    Widget.find({},{}, { limit: req.query.limit ? req.query.limit : null,
+    Widget.find({}, {}, { limit: req.query.limit ? req.query.limit : null,
         sort: req.query.sort ? req.query.sort : "size",
         skip: req.query.skip ? req.query.skip : null }, function (err, widget) {
         if (err) {
@@ -31,14 +43,14 @@ function action_list_widgets(req, res, next) {
 }
 
 function action_delete_widget(req, res) {
-    if(!req.params.id){
+    if (!req.params.id) {
         return res.status(400).send({error: "fileid required"});
     }
     Widget.findByIdAndRemove(req.params.id, function (err, file) {
         if (!err) {
             res.send("successfully file deleted" + file);
-        }else{
-            res.send("error in removing user"+ err);
+        } else {
+            res.send("error in removing user" + err);
         }
 
     });
