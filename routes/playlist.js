e3cd5f8dff0ea93dd702a3eb965dd7ed33fd771e
ayyappaappana <ayyappa.appana@mtuity.com>
2016-01-04 19:17:22 +0530

    playlist add

--- routes/playlist.js
+++ routes/playlist.js
@@ -17,20 +17,21 @@ router.put('/edit/:id', action_edit_playlist);
 function action_save_playlist(req, res) {
 
     var playlist = new Playlist(req.body);
-    playlist.save(function (err, playlist) {
+    playlist.save(function(err, playlist) {
         if (err || !playlist) {
             res.status(500).send(err);
-        }
-        else {
+        } else {
             return res.json(playlist);
         }
     });
 }
 
 function action_list_playlist(req, res, next) {
-    Playlist.find({}, {}, { limit: req.query.limit ? req.query.limit : null,
+    Playlist.find({}, {}, {
+        limit: req.query.limit ? req.query.limit : null,
         sort: req.query.sort ? req.query.sort : "size",
-        skip: req.query.skip ? req.query.skip : null }, function (err, playlist) {
+        skip: req.query.skip ? req.query.skip : null
+    }, function(err, playlist) {
         if (err) {
             res.json(err);
         } else {
@@ -41,20 +42,28 @@ function action_list_playlist(req, res, next) {
 
 function action_remove_playlist(req, res) {
     if (!req.params.id) {
-        return res.status(400).send({error: "fileid required"});
+        return res.status(400).send({
+            error: "fileid required"
+        });
     }
-    Playlist.findByIdAndRemove(req.params.id, function (err, file) {
+    Playlist.findByIdAndRemove(req.params.id, function(err, file) {
         if (!err) {
-            res.send("successfully file deleted" + file);
+            res.json({
+                "msg": "successfully file deleted" + file
+            });
         } else {
-            res.send("error in removing user" + err);
+            res.json(err);
         }
 
     });
 }
 
-function action_edit_playlist(req, res){
-    Playlist.findOneAndUpdate({_id: req.params.id}, req.body, {upsert: true}, function (err, playlist) {
+function action_edit_playlist(req, res) {
+    Playlist.findOneAndUpdate({
+        _id: req.params.id
+    }, req.body, {
+        upsert: true
+    }, function(err, playlist) {
         if (!err) {
             return res.status(200).send("Successfully Updated");
         } else {
@@ -65,5 +74,3 @@ function action_edit_playlist(req, res){
 
 
 module.exports = router;
-
-
