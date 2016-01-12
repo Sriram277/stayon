10f10f15c9cedc9297a60ccd831358b68bd33bba
ayyappaappana <ayyappa.appana@mtuity.com>
2016-01-12 18:30:04 +0530

    schedulars

--- models/playlist.js
+++ models/playlist.js
@@ -1,5 +1,4 @@
-
-module.exports = function (mongoose) {
+module.exports = function(mongoose) {
     "use strict";
     var Schema = mongoose.Schema;
 
@@ -7,12 +6,23 @@ module.exports = function (mongoose) {
         useridfk: String,
         widgetname: String,
         type: String,
-        embbedcode:String,
-        settings:{type: Object} ,
-        thumbnailurl:String,
-        created_on: {type: Date, "default": Date.now},
-        updated_on: {type: Date, "default": Date.now}
-    },{ strict: false });
+        embbedcode: String,
+        settings: {
+            type: Object
+        },
+        thumbnailurl: String,
+        play_list: Array,
+        created_on: {
+            type: Date,
+            "default": Date.now
+        },
+        updated_on: {
+            type: Date,
+            "default": Date.now
+        }
+    }, {
+        strict: false
+    });
 
     var Playlist = mongoose.model('Playlist', playlistSchema);
 
--- routes/schedular.js
+++ routes/schedular.js
@@ -5,6 +5,7 @@ var global = require('../config/global.js');
 
 var mongoose = require('mongoose');
 var Schedular = mongoose.model('Schedular');
+var Playlist = mongoose.model('Playlist');
 
 
 
@@ -16,37 +17,97 @@ router.delete('/delete/:id', action_delete_schedular);
 
 router.put('/edit/:id', action_edit_schedular);
 
-router.get('/get/:id' , action_getone_schedular);
+router.get('/get/:id', action_getone_schedular);
 
 function action_save_schedular(req, res) {
     var reqBody = req.body;
-    console.log(reqBody);
-    if(!reqBody){
-        return res.status(400).send({error:"Body should not be empty"});
+    if (!reqBody) {
+        return res.status(400).send({
+            error: "Body should not be empty"
+        });
     }
-    
+
     var schedular = new Schedular(req.body);
-    schedular.save(function (err, doc) {
-    	if(err) {
-    		res.json(err);
-    	}else {
-            var displays =  reqBody.displays;
-            if(displays){
-                _.each(displays, function (display, index) {
-                    global.clients[display].emit('ping');
-                    if(displays.length = index +1){
-                        res.json(doc);
+    schedular.save(function(err, doc) {
+        if (err) {
+            res.json(err);
+        } else {
+            var starttime = new Date(doc.start_time);
+            var endtime = new Date(doc.start_time);
+            var finalObj = {};
+
+            var playerlist = [];
+            finalObj.schedular_id = doc._id;
+            Playlist.findOne({
+                "_id": doc.playlist_id
+            }, function(err, result) {
+                var play_list = result.play_list;
+                _.each(play_list, function(list, count) {
+                    endtime = new Date(endtime.getTime() + list.duration * 60 * 1000);
+                    //console.log(endtime + "---------" + list.duration);
+                    playerlist.push({
+                        "start_time": starttime,
+                        "end_time": endtime,
+                        "location": list.location,
+                        "filetype": list.filetype
+                    });
+                    starttime = new Date(starttime.getTime() + list.duration * 60 * 1000);
+                    if (play_list.length === count + 1) {
+                        //console.log(playerlist);
+                        finalObj.medialist = playerlist;
+                        console.log(finalObj);
+                        var displays = reqBody.displays;
+                        if (displays) {
+                            _.each(displays, function(display, index) {
+                                console.log(display);
+                                if (global.clients[display]) {
+                                    global.clients[display].emit('scheduledlist', finalObj);
+                                }
+                                // if (displays.length = index + 1) {
+
+                                //}
+                            });
+                        }
                     }
                 });
-            }
-    	}
+                res.json(doc);
+            });
+
+        }
     });
 }
 
+// function action_save_schedular(req, res) {
+//     var reqBody = req.body;
+//     console.log(reqBody);
+//     if(!reqBody){
+//         return res.status(400).send({error:"Body should not be empty"});
+//     }
+
+//     var schedular = new Schedular(req.body);
+//     schedular.save(function (err, doc) {
+//      if(err) {
+//          res.json(err);
+//      }else {
+//             var displays =  reqBody.displays;
+//             if(displays){
+//                 _.each(displays, function (display, index) {
+//                     global.clients[display].emit('ping');
+//                     if(displays.length = index +1){
+//                         res.json(doc);
+//                     }
+//                 });
+//             }
+//      }
+//     });
+// }
+
 function action_list_schedular(req, res, next) {
-    Schedular.find({}, {}, { limit: req.query.limit ? req.query.limit : null,
+    Schedular.find({}, {}, {
+        limit: req.query.limit ? req.query.limit : null,
         sort: req.query.sort ? req.query.sort : "size",
-        skip: req.query.skip ? req.query.skip : null }, function (err, schedular) {
+        skip: req.query.skip ? req.query.skip : null
+    }, function(err, schedular) {
         if (err) {
             res.json(err);
         } else {
@@ -57,18 +118,20 @@ function action_list_schedular(req, res, next) {
 
 
 function action_delete_schedular(req, res) {
-	if (!req.params.id) {
-		return res.status(400).send({error:"Something wrong"});
-	}
-	Schedular.findByIdAndRemove(req.params.id, function (err, doc) {
-		 if (!err) {
+    if (!req.params.id) {
+        return res.status(400).send({
+            error: "Something wrong"
+        });
+    }
+    Schedular.findByIdAndRemove(req.params.id, function(err, doc) {
+        if (!err) {
             res.json({
                 "msg": "successfully file deleted"
             });
         } else {
             res.json(err);
         }
-	});
+    });
 }
 
 
@@ -88,15 +151,15 @@ function action_edit_schedular(req, res) {
 
 function action_getone_schedular(req, res) {
     var schedular_id = req.params.id;
-    Schedular.findOne({"_id" :  schedular_id} , function(err, schedular) {
-        if(err) {
+    Schedular.findOne({
+        "_id": schedular_id
+    }, function(err, schedular) {
+        if (err) {
             res.json(err);
-        }else{
+        } else {
             res.json(schedular);
         }
     });
 }
 
 module.exports = router;
-
-
